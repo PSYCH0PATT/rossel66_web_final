cb848307762cc8ab17c2f63babe1761802485738
macbook <macbook@macbooks-MacBook-Air.local>
2025-05-27 00:58:31 +0200

    Норм билд серва4

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Для __dirname в ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => { // Убираем неиспользуемые параметры
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+  // Если у вас есть другие специфичные для проекта настройки Next.js,
+  // их нужно будет добавить сюда.
+  // reactStrictMode: true, // Например
   reactStrictMode: true,
   images: {
     domains: [
@@ -7,6 +21,6 @@ const nextConfig = {
       'hebbkx1anhila5yf.public.blob.vercel-storage.com'
     ],
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig; // Используем ES Module экспорт
